45abb899012f6f6285cf1094f55b4813e9ea6aea
mikemacharia39 <mikemacharia39@gmail.com>
2022-06-01 19:50:09 +0300

    define interface for student service

--- src/main/java/com/mikehenry/graphqlclientdemo/service/StudentService.java
+++ src/main/java/com/mikehenry/graphqlclientdemo/service/StudentService.java
@@ -1,2 +1,11 @@
-package com.mikehenry.graphqlclientdemo.service;public interface StudentService {
+package com.mikehenry.graphqlclientdemo.service;
+
+import com.mikehenry.graphqlclientdemo.request.CreateStudentRequest;
+import com.mikehenry.graphqlclientdemo.response.StudentResponse;
+
+public interface StudentService {
+
+    StudentResponse getStudent(Long id);
+
+    StudentResponse createStudent(CreateStudentRequest createStudentRequest);
 }
