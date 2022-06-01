fcbe73d5659e7ee73c98330d41d80aef4b9de62b
mikemacharia39 <mikemacharia39@gmail.com>
2022-06-01 18:46:05 +0300

    new graphql client setup

---
+++ src/main/java/com/mikehenry/graphqlclientdemo/GraphqlClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mikehenry.graphqlclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlClientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikehenry/graphqlclientdemo/GraphqlClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikehenry.graphqlclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
